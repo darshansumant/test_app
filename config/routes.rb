d54a3c80b2d857becd9e62d19d3abc832d1e0938
darshansumant <darshan.sumant@gmail.com>
2019-06-10 19:20:35 -0500

    root updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :dogs
   resources :drinks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Routing to the default page
+  root 'dogs#index'
 end
